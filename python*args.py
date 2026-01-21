7b41a463a7345328144009a5a0232350c75139fa
swarupchaudhari <chaudhariswarup266@gmail.com>
2026-01-21 07:43:15 +0530

    Update python*args and add kwargs.py

--- python*args.py
+++ python*args.py
@@ -9,3 +9,22 @@ def sum(*args):
 
 
 print(sum(342, 2, 7, 9))
+
+
+
+  #kwargs
+def marks(**kwargs):
+    # kwargs is a dictionary of key-value pairs
+    for item in kwargs.keys():
+        print(f"The marks of {item} is {kwargs[item]}")
+
+marks(shubham=34, vikrant=54, jack=34, marie=90, priya=45)
+
+
+
+#args_kwargs.py
+def func1(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+func1(1, 2, 4, 5, jack=54, joyboy=24, marin=29)
