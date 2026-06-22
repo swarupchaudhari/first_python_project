7b4760119c5cef1846c4d2716b8d5e0274bb7d9f
swarupchaudhari <chaudhariswarup266@gmail.com>
2026-06-22 08:49:12 +0530

    Update app.jsx

--- react_start /props /app.jsx
+++ react_start /props /app.jsx
@@ -4,9 +4,23 @@ import Card from './components/Card';
 const App = () => {
   return (
     <div className="parent">
-      <Card user="Swarup" age={20} />
-      <Card user="Sahil" age={21} />
-      <Card user="Karan" age={17} />
+      <Card
+        user="Swarup"
+        age={20}
+        img="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500"
+      />
+
+      <Card
+        user="Sahil"
+        age={21}
+        img="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=500"
+      />
+
+      <Card
+        user="Karan"
+        age={17}
+        img="https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500"
+      />
     </div>
   );
 };
