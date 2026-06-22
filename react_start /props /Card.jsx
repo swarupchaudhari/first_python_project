66f51c050f1bc205e303ba108d7e087f3b05f92c
swarupchaudhari <chaudhariswarup266@gmail.com>
2026-06-22 08:19:13 +0530

    Create Card.jsx

---
+++ react_start /props /Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="card">
+      <img
+        src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500"
+        alt="Profile"
+      />
+
+      <h1>Swarup Chaudhari</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+
+      <button>View Profile</button>
+    </div>
+  );
+};
+
+export default Card;
