956f96e4c6ec378daf97512771523a9f3dc0ce62
swarupchaudhari <chaudhariswarup266@gmail.com>
2026-06-28 08:42:25 +0530

    Create useState /app.jsx

---
+++ react_start /useState /app.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+
+  function increaseNum() {
+    setNum(num + 1);
+  }
+
+  function decreaseNum() {
+    setNum(num - 1);
+  }
+
+  function jump5Num() {
+    setNum(num + 5);
+  }
+
+  return (
+    <div>
+      <h1>{num}</h1>
+
+      <button onClick={increaseNum}>Increase</button>
+
+      <button onClick={decreaseNum}>Decrease</button>
+
+      <button onClick={jump5Num}>Increase by 5</button>
+    </div>
+  );
+};
+
+export default App;
